ffef0b734d732a7532f833ee0d1af17385358ccd
clement <clem.jambou@gmail.com>
2022-02-21 19:02:46 +0100

    updated for 2022

--- linesheets/run.bash
+++ linesheets/run.bash
@@ -1,12 +1,17 @@
 #!/bin/bash
 python3 linesheet.py 
 
+rm -r pdfs
+cp -r photos output/photos
 cd output
 for filename in *.tex; do
-    pdflatex "$filename" -interaction=nonstopmode
+    pdflatex "$filename" --interaction=nonstopmode
 done
 cd ..
 mkdir pdfs
 mv output/*.pdf pdfs/
 rm output/*.log 
 rm output/*.aux
+
+mv pdfs/Blush.pdf . 
+pdftk front.pdf Blush.pdf pdfs/*.pdf output linesheet.pdf
